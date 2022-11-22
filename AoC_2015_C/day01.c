3f2dcc2533071c2b3bf90f671edb32a8e177395a
sarwoo <spiral464@gmail.com>
2022-11-22 06:29:43 +0000

    added ignore

--- AoC_2015_C/day01.c
+++ AoC_2015_C/day01.c
@@ -1,6 +1,52 @@
+/*
+--- Day 1: Not Quite Lisp ---
+
+Santa was hoping for a white Christmas, but his weather machine's "snow" function is powered by stars, 
+and he's fresh out! To save Christmas, he needs you to collect fifty stars by December 25th.
+
+Collect stars by helping Santa solve puzzles. Two puzzles will be made available on each day in the Advent 
+calendar; the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
+
+Here's an easy 
+puzzle to warm you up.
+
+Santa is trying to deliver presents in a large apartment building, but he can't find the right floor - the 
+directions he got are a little confusing. He starts on the ground floor (floor 0) and then follows the instructions 
+one character at a time.
+
+An opening parenthesis, (, means he should go up one floor, and a closing parenthesis, ), means he should go down one floor.
+
+The apartment building is very tall, and the basement is very deep; he will never find the top or bottom floors.
+
+For example:
+
+    (()) and ()() both result in floor 0.
+    ((( and (()(()( both result in floor 3.
+    ))((((( also results in floor 3.
+    ()) and ))( both result in floor -1 (the first basement level).
+    ))) and )())()) both result in floor -3.
+
+To what floor do the instructions take Santa?
+
+--- Part Two ---
+
+Now, given the same instructions, find the position of the first character that causes him to enter the 
+basement (floor -1). The first character in the instructions has position 1, the second character has position 2, and so on.
+
+For example:
+
+    ) causes him to enter the basement at character position 1.
+    ()()) causes him to enter the basement at character position 5.
+
+What is the position of the character that causes Santa to first enter the basement?
+
+*/
+
 #include <stdio.h>
 
-#define DATAFILE "data/day01.txt"
+#define DATAFILE "data_day01.txt"
+#define PART1 280
+#define PART2 1797
 
 int file_size(char *filename) {
     FILE *fh;
@@ -46,7 +92,7 @@ void part_1(char *data, int size) {
         //     level--;
         // }
     }
-    printf("Santa is on level %i  [280]\n", level);
+    printf("Santa finished on level %i  [%s]\n", level, level == PART1 ? "true" : "false");
 }
 
 void part_2(char *data, int size) {
@@ -60,15 +106,16 @@ void part_2(char *data, int size) {
             count++;
         }
     }
-    printf("Santa reached basement on move %i  [1797]\n", count);
+
+    printf("Santa reached the basement on move %i  [%s]\n", count, count == PART2  ? "true" : "false");
 }
 
 int main(void) {
-    printf("Advent of code - 2015 - Day 1\n");
+    printf("--- Day 1: Not Quite Lisp ---\n");
 
     // Find size of datafile
     int size = file_size(DATAFILE);
-    printf("Size of data: %i\n", size);
+    // printf("Size of data: %i\n", size);
 
     // initialise array to size of data
     char data[size];
